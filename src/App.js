382c1b4b30f384693fcae6ea846359a6f1bfc225
Rahul Jain <rahuljait@gmail.com>
2022-05-20 20:42:09 +0530

    deployed over git

--- src/App.js
+++ src/App.js
@@ -1,81 +1,84 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import {Checkbox} from "./Checkbox"
+import { Checkbox } from "./Checkbox";
 
-
-const App=()=> {
+const App = () => {
   const [data, setData] = useState([]);
-  const [goal, setGoal] = useState('');
+  const [goal, setGoal] = useState("");
   // const [togoal, setTogoal] = useState(data)
-  
+
   useEffect(() => {
-    fetch("http://localhost:8000/api/goals/")
+    fetch("https://rahuljaim-api-express-mongodb.herokuapp.com/api/goals/")
       .then((response) => response.json())
       .then((json) => setData(json));
   }, []);
 
   ///////////////////////////////
-  const submitHandler =(e)=>{
-    fetch("http://localhost:8000/api/goals/", {
-  method: "post",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    text:goal
-}),
-})
-.then( (response) => response.json()
-   //do something awesome that makes the world a better place
-).then (json =>console.log(json));
-  }
-////////////////////////////////////
-  const deleteTask =(e)=>{
-    fetch(`http://localhost:8000/api/goals/${e}`, {
-  method: "delete",
-})
-.then( (response) => response.json()
-).then (json =>console.log("json ",json));
+  const submitHandler = (e) => {
+    fetch("https://rahuljaim-api-express-mongodb.herokuapp.com/api/goals/", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: goal,
+      }),
+    })
+      .then(
+        (response) => response.json()
+        //do something awesome that makes the world a better place
+      )
+      .then((json) => console.log(json));
+  };
+  ////////////////////////////////////
+  const deleteTask = (e) => {
+    fetch(
+      `https://rahuljaim-api-express-mongodb.herokuapp.com/api/goals/${e}`,
+      {
+        method: "delete",
+      }
+    )
+      .then((response) => response.json())
+      .then((json) => console.log("json ", json));
 
-let updatedData = data.filter(item=>item._id !==e);
-setData(updatedData);
-  }
- 
-//////////////////////////////////////
-  const updateCheckStatus = index => {
+    let updatedData = data.filter((item) => item._id !== e);
+    setData(updatedData);
+  };
+
+  //////////////////////////////////////
+  const updateCheckStatus = (index) => {
     setData(
       data.map((goal) =>
-        goal._id === index
-          ? { ...goal, checked: !goal.checked }
-          : goal
+        goal._id === index ? { ...goal, checked: !goal.checked } : goal
       )
-    )
-  }
+    );
+  };
   //////////////////////////////////////
   const updateTask = (goal_id, newdata) => {
-    fetch(`http://localhost:8000/api/goals/${goal_id}`, {
-  method: "put",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    text:newdata
-}),
-})
-.then( (response) => response.json()
-).then (json =>console.log("json ",json));
+    fetch(
+      `https://rahuljaim-api-express-mongodb.herokuapp.com/api/goals/${goal_id}`,
+      {
+        method: "put",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: newdata,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((json) => console.log("json ", json));
 
-// let updatedData = data.filter(item=>item._id !==goal_id);
-// setData(updatedData);
-  }
+    // let updatedData = data.filter(item=>item._id !==goal_id);
+    // setData(updatedData);
+  };
 
   ////////////////////////////////////////////
   return (
     <div className="App">
-      
-
       {data.map((goal) => (
         <Checkbox
           key={goal._id}
@@ -83,17 +86,23 @@ setData(updatedData);
           checkHandler={() => updateCheckStatus(goal._id)}
           label={goal.text}
           index={goal._id}
-          deleteHandler ={()=>deleteTask(goal._id)}
-          submitHandler ={(newdata)=>updateTask(goal._id,newdata)}
+          deleteHandler={() => deleteTask(goal._id)}
+          submitHandler={(newdata) => updateTask(goal._id, newdata)}
         />
       ))}
-        
+
       <form>
-        <input type='text' value = {goal} onChange={(event)=>setGoal(event.target.value)}/>
-        <button type="submit" onClick={submitHandler} >Submit</button>
+        <input
+          type="text"
+          value={goal}
+          onChange={(event) => setGoal(event.target.value)}
+        />
+        <button type="submit" onClick={submitHandler}>
+          Submit
+        </button>
       </form>
     </div>
   );
-}
+};
 
-export default App
+export default App;
